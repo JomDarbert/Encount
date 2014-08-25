ba2c5eed14004cddab2c85fadeda8ccf4e6f9c91
JomDarbert <ta.herbert@gmail.com>
2014-08-25 15:16:28 -0700

    Updated boilerplate

--- ember/app.coffee
+++ ember/app.coffee
@@ -1,30 +1,8 @@
+window.App = Ember.Application.create()
 ###
-To Do:
-upgrade encryption for passwords
-archive events
-count attendance, other stats?
-save composition
-calculate off-role
-add cookies
-###
-
-window.App = Ember.Application.create(
-)
-
 App.ApplicationAdapter = DS.FirebaseAdapter.extend(
   firebase: new Firebase('https://ptp-events.firebaseio.com')
 )
 
 App.ApplicationSerializer = DS.FirebaseSerializer.extend()
-
-# HELPERS
-Ember.Handlebars.helper "datetime", (value, options) ->
-  format = "MMM. D, YYYY [@] h:mma"
-  format = options.hash.format  if options.hash.format
-  if value
-    time = moment(value).format(format)
-    new Handlebars.SafeString(time)
-
-
-
-App.ChildView = Ember.View.extend()
+###
--- ember/app.js
+++ ember/app.js
@@ -1,31 +1,10 @@
-
-/*
-To Do:
-upgrade encryption for passwords
-archive events
-count attendance, other stats?
-save composition
-calculate off-role
-add cookies
- */
 window.App = Ember.Application.create();
 
-App.ApplicationAdapter = DS.FirebaseAdapter.extend({
+
+/*
+App.ApplicationAdapter = DS.FirebaseAdapter.extend(
   firebase: new Firebase('https://ptp-events.firebaseio.com')
-});
+)
 
-App.ApplicationSerializer = DS.FirebaseSerializer.extend();
-
-Ember.Handlebars.helper("datetime", function(value, options) {
-  var format, time;
-  format = "MMM. D, YYYY [@] h:mma";
-  if (options.hash.format) {
-    format = options.hash.format;
-  }
-  if (value) {
-    time = moment(value).format(format);
-    return new Handlebars.SafeString(time);
-  }
-});
-
-App.ChildView = Ember.View.extend();
+App.ApplicationSerializer = DS.FirebaseSerializer.extend()
+ */
